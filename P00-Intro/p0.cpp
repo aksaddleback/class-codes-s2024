63daa85c47bde8643f87cfb95237ac5297e069b4
aksaddleback <142381968+aksaddleback@users.noreply.github.com>
2024-06-06 16:03:07 +0000

    multi-line comment

--- P00-Intro/p0.cpp
+++ P00-Intro/p0.cpp
@@ -2,12 +2,14 @@
 // comments are not c++ codes and only are for explaining and write sentences. 
 // the following two lines always comes first and we will learn about them later
 #include<iostream>
-using namespace std; 
-
+using namespace std;
 // this is the main function of the code. what we put inside { and } will run as our program
 int main() { 
-    // here we will write the statements 
-    // cout is the frist statement we learn in this class. it stands for console out.
-    // it prints what we want on the console (terminal)
+    /*
+     here we will write the statements 
+     cout is the frist statement we learn in this class. it stands for console out.
+     it prints what we want on the console (terminal)
+    */
+    cout << "Hello C++ World. This is my first C++ program!\n";
     cout << "Hello C++ World. This is my first C++ program!\n";
 }
