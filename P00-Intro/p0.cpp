4991cab44d4101eb4c85fc0b310e490953a226c3
aksaddleback <142381968+aksaddleback@users.noreply.github.com>
2024-06-06 15:14:33 +0000

    june 5 changes

---
+++ P00-Intro/p0.cpp
@@ -0,0 +1,13 @@
+// this is how we write comments in our code. that means lines starting with // are going to be ignored by compiler. 
+// comments are not c++ codes and only are for explaining and write sentences. 
+// the following two lines always comes first and we will learn about them later
+#include<iostream>
+using namespace std; 
+
+// this is the main function of the code. what we put inside { and } will run as our program
+int main() { 
+    // here we will write the statements 
+    // cout is the frist statement we learn in this class. it stands for console out.
+    // it prints what we want on the console (terminal)
+    cout << "Hello C++ World. This is my first C++ program!\n";
+}
